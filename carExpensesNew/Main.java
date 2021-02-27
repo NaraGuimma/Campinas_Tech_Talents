c09fd9c6c6d855b7283f4b98169d7340a0d7f393
Nara Guimaraes <60903424+NaraGuimma@users.noreply.github.com>
2021-02-27 12:07:38 -0300

    Create Main.java

---
+++ carExpensesNew/Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    
+    Opcoes op = new Opcoes();
+
+    System.out.println("Bem vindo ao cálculo médio do seu automóvel! Vamos começar\n");
+
+    op.Options();
+
+
+    System.out.println("\nVolte sempre!!!!\n");
+    
+    
+
+  }
+}
